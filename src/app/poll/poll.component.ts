1de17536c3e8fdff15f26acc8da8b5437924a8fe
moemen99 <moerawaq@gmail.com>
2025-09-27 13:10:40 +0300

    Adding Bootstrap and font awesome and displaying data

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -12,6 +12,8 @@ export class PollComponent {
   count ;
   imgUrl='https://images.unsplash.com/photo-1756680969191-a453442682f9?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   colSpan = 2;
+  isActive=true;
+  mainColor='pink'
   constructor(pollService : PollService){
     this.polls = pollService.getPolls();
     this.count = this.polls.length
